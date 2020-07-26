0f7ffc4bcd98a7b82fff5abc2fc056e8ec4d9222
mr-poston <60262786+mr-poston@users.noreply.github.com>
2020-07-25 20:39:50 -0500

    Update __init__.py

--- example/__init__.py
+++ example/__init__.py
@@ -2,5 +2,5 @@ import check50
 
 @check50.check()
 def hello_world():
-  """hello world"""
-  check50.run(python3 hello.py).stdout("Hello, world!", regex=False).exit(0)
+    """hello world"""
+    check50.run("python3 hello.py").stdout("Hello, world!", regex=False).exit(0)
