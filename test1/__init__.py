af116f0568b555bfc835a1af5c807c42abaa7017
mr-poston <60262786+mr-poston@users.noreply.github.com>
2020-07-26 17:00:00 -0500

    Update __init__.py

--- test1/__init__.py
+++ test1/__init__.py
@@ -6,6 +6,16 @@ def exists():
   check50.exists("IteratorRemover.java", "IteratorRemoverRunner.java")
   
 @check50.check(exists)
+def check0():
+  """IteratorRemover.java compiles"""
+  check50.run("javac IteratorRemover.java").exit(0)
+  
+@check50.check(exists)
 def check1():
+  """IteratorRemoverRunner.java compiles"""
+  check50.run("javac IteratorRemoverRunner.java").exit(0)
+  
+@check50.check(exists)
+def check2():
   """"a b c a b c a   a -> [b, c, b, c]"""
   check50.run("java IteratorRemoverRunner").stdout("[b, c, b, c]").exit(1)
