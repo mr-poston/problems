3f205c82b4b81dca22b34504a17a91d7385e1a10
mr-poston <60262786+mr-poston@users.noreply.github.com>
2020-07-25 21:04:13 -0500

    Create __init__.py

---
+++ test1/__init__.py
@@ -0,0 +1,11 @@
+import check50
+
+@check50.check()
+def exists_ir():
+  """IteratorRemover.java exists"""
+  check50.exists("IteratorRemover.java")
+  
+@check50.check()
+def exists_irr():
+  """IteratorRemoverRunner.java exists"""
+  check50.exists("IteratorRemoverRunner.java)
