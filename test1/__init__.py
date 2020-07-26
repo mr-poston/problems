1358c3fc27a166bbc3421305a935948dd08def9b
mr-poston <60262786+mr-poston@users.noreply.github.com>
2020-07-25 21:26:44 -0500

    Update __init__.py

--- test1/__init__.py
+++ test1/__init__.py
@@ -1,6 +1,11 @@
 import check50
 
 @check50.check()
-def exists():
-  """IteratorRemover.java exists"""
+def exists_ir():
+  """IteratorRemover.java and IteratorRemoverRunner.java exist"""
   check50.exists("IteratorRemover.java", "IteratorRemoverRunner.java")
+  
+@check50.check(exists)
+def compiles():
+  """IteratorRemover.java compiles"""
+  check50.java.compile("IteratorRemover.java")
