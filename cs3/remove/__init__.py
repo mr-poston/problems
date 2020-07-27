aeb1f3c5c45492f5998918d231b14d3da43fba00
mr-poston <john.poston@risd.org>
2020-07-27 18:11:38 +0000

    added cs3 folder

---
+++ cs3/remove/__init__.py
@@ -0,0 +1,44 @@
+import check50
+
+@check50.check()
+def exists():
+  """IteratorRemover.java and IteratorRemoverRunner.java exist"""
+  check50.exists("IteratorRemover.java", "IteratorRemoverRunner.java")
+  
+@check50.check(exists)
+def check0():
+  """IteratorRemover.java compiles"""
+  check50.run("javac IteratorRemover.java").exit(0)
+  
+@check50.check(exists)
+def check1():
+  """IteratorRemoverRunner.java compiles"""
+  check50.run("javac IteratorRemoverRunner.java").exit(0)
+  
+@check50.check(exists)
+def check2():
+  """Example #1"""
+  check50.run("java IRTest \"a b c a b c a\" \"a\"").stdout("[b, c, b, c]", regex=False).exit(0)
+
+@check50.check(exists)
+def check3():
+  """Example #2"""
+  check50.run("java IRTest \"a b c d e f g h i j x x x x\" \"x\"").stdout("[a, b, c, d, e, f, g, h, i, j]", regex=False).exit(0)
+  
+@check50.check(exists)
+def check4():
+  """Example #3"""
+  check50.run("java IRTest \"1 2 3 4 5 6 a b c a b c\" \"b\"").stdout("[1, 2, 3, 4, 5, 6, a, c, a, c]", regex=False).exit(0)
+  
+@check50.check(exists)
+def check5():
+  """Handles empty list"""
+  check50.run("java IRTest \"\" \"\"").stdout("[]", regex=False).exit(0)
+
+@check50.check(exists)
+def check6():
+  """Uses Iterator"""
+  f = open("IteratorRemover.java", "r")
+  contents = f.read()
+  if contents.find(".iterator()") == -1:
+    raise check50.Failure("You must use an Iterator")
