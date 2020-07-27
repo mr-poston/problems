0a31f1039cd51f4f3b77486c52fa3f63ba3c30db
mr-poston <60262786+mr-poston@users.noreply.github.com>
2020-07-27 10:45:04 -0500

    Update __init__.py

--- test1/__init__.py
+++ test1/__init__.py
@@ -18,12 +18,12 @@ def check1():
 @check50.check(exists)
 def check2():
   """Example #1"""
-  check50.run("java IRTest \"a b c a b c a\" \"a\"").stdout("[b, c, b, c]")
+  check50.run("java IRTest \"a b c a b c a\" \"a\"").stdout("[b, c, b, c]", regex=False)
 
 @check50.check(exists)
 def check3():
   """Example #2"""
-  check50.run("java IRTest \"a b c d e f g h i j x x x x\" \"x\"").stdout("[a, b, c, d, e, f, g, h, i, j]").exit(0)
+  check50.run("java IRTest \"a b c d e f g h i j x x x x\" \"x\"").stdout("[a, b, c, d, e, f, g, h, i, j]", regex=False).exit(0)
 
 @check50.check(exists)
 def check4():
