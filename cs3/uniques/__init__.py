14e9a9840abb54c7db0a453d72f647926dbe5e5f
mr-poston <john.poston@risd.org>
2020-08-03 20:01:44 +0000

    Update __init__.py

--- cs3/uniques/__init__.py
+++ cs3/uniques/__init__.py
@@ -18,16 +18,10 @@ def runner_compiles():
 @check50.check(exists)
 def example1():
     """Example #1"""
-		out1 = "a b c d e f g h a b c d e f g h i j k"
-		out2 = "[a, b, c, d, e, f, g, h, i, j, k]"
-		out3 = "[a, b, c, d, e, f, g, h]"
     check50.run("java Grader \"a b c d e f g h a b c d e f g h i j k\"").stdout("a b c d e f g h a b c d e f g h i j k\n[a, b, c, d, e, f, g, h, i, j, k]\n[a, b, c, d, e, f, g, h]", regex=False).exit(0)
 
 @check50.check(exists)
 def example2():
 		"""Example #2"""
-		out1 = "one two three one two three six seven one two"
-		out2 = "[one, seven, six, three, two]"
-		out3 = "[one, three, two]"
 		check50.run("java Grader \"one two three one two three six seven one two\"").stdout("one two three one two three six seven one two\n[one, seven, six, three, two]\n[one, three, two]", regex=False).exit(0)
 		
