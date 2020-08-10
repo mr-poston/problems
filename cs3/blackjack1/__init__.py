4bca4b1a486af3163de6999214be6d88c0dcf4a5
mr-poston <john.poston@risd.org>
2020-08-10 13:45:25 +0000

    Added Test Cases

--- cs3/blackjack1/__init__.py
+++ cs3/blackjack1/__init__.py
@@ -29,3 +29,34 @@ def blackjackcard_compiles():
 def cardtestone_compiles():
     """CardTestOne.java compiles"""
     check50.run("javac CardTestOne.java").exit(0)
+
+@check50.check()
+def blank_card():
+    """Blank card produces a value of 0"""
+    check50.run("java Grader").stdout("ZERO of  | value = 0", regex=False).exit(0)
+
+@check50.check()
+def ace_diamonds():
+    """Ace of Diamonds is correct"""
+    check50.run("java Grader \"1 DIAMONDS\"").stdout("ACE of DIMONDS | value = 11", regex=False).exit(0)
+
+@check50.check()
+def four_clubs():
+    """Four of Clubs is correct"""
+    check50.run("java Grader \"4 CLUBS\"").stdout("FOUR of CLUBS | value = 4", regex=False).exit(0)
+
+@check50.check()
+def queen_spades():
+    """Queen of Spades is correct"""
+    check50.run("java Grader \"12 SPADES\"").stdout("QUEEN of SPADES | value = 10", regex=False).exit(0)
+
+@check50.check()
+def queen_hearts():
+    """Queen of HEARTS is correct"""
+    check50.run("java Grader \"12 HEARTS\"").stdout("QUEEN of HEARTS | value = 10", regex=False).exit(0)
+
+@check50.check()
+def nine_spades():
+    """Nine of SPADES is correct"""
+    check50.run("java Grader \"9 SPADES\"").stdout("NINE of SPADES | value = 9", regex=False).exit(0)
+
