15d519981922fc30770c2c2e9ab9d4dd1d87dc9b
mr-poston <john.poston@risd.org>
2020-10-14 20:51:18 +0000

    Update Remove

--- cs3/remove/__init__.py
+++ cs3/remove/__init__.py
@@ -4,36 +4,36 @@ import check50
 def exists():
   """IteratorRemover.java and IteratorRemoverRunner.java exist"""
   check50.exists("IteratorRemover.java", "IteratorRemoverRunner.java")
-  
+
 @check50.check(exists)
 def check0():
   """IteratorRemover.java compiles"""
   check50.run("javac IteratorRemover.java").exit(0)
-  
+
 @check50.check(exists)
 def check1():
   """IteratorRemoverRunner.java compiles"""
   check50.run("javac IteratorRemoverRunner.java").exit(0)
-  
+
 @check50.check(exists)
 def check2():
   """Example #1"""
-  check50.run("java IRTest \"a b c a b c a\" \"a\"").stdout("[b, c, b, c]", regex=False).exit(0)
+  check50.run("java Grader \"a b c a b c a\" \"a\"").stdout("[b, c, b, c]", regex=False).exit(0)
 
 @check50.check(exists)
 def check3():
   """Example #2"""
-  check50.run("java IRTest \"a b c d e f g h i j x x x x\" \"x\"").stdout("[a, b, c, d, e, f, g, h, i, j]", regex=False).exit(0)
-  
+  check50.run("java Grader \"a b c d e f g h i j x x x x\" \"x\"").stdout("[a, b, c, d, e, f, g, h, i, j]", regex=False).exit(0)
+
 @check50.check(exists)
 def check4():
   """Example #3"""
-  check50.run("java IRTest \"1 2 3 4 5 6 a b c a b c\" \"b\"").stdout("[1, 2, 3, 4, 5, 6, a, c, a, c]", regex=False).exit(0)
-  
+  check50.run("java Grader \"1 2 3 4 5 6 a b c a b c\" \"b\"").stdout("[1, 2, 3, 4, 5, 6, a, c, a, c]", regex=False).exit(0)
+
 @check50.check(exists)
 def check5():
   """Handles empty list"""
-  check50.run("java IRTest \"\" \"\"").stdout("[]", regex=False).exit(0)
+  check50.run("java Grader \"\" \"\"").stdout("[]", regex=False).exit(0)
 
 @check50.check(exists)
 def check6():
