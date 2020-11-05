28cc827a64feec231319930e4972473fdf636ab7
mr-poston <60262786+mr-poston@users.noreply.github.com>
2020-11-05 14:46:36 -0600

    Update __init__.py

--- cs3/mathset/__init__.py
+++ cs3/mathset/__init__.py
@@ -23,24 +23,24 @@ def runner_compiles():
 @check50.check()
 def example1():
     """Example 1"""
-    check50.run("java Grader\"1 2 3 4 5\" \"4 5 6 7 8\"")\
+    check50.run("java Grader \"1 2 3 4 5\" \"4 5 6 7 8\"")\
             .stdout("[1, 2, 3, 4, 5, 6, 7, 8]\n[4, 5]\n[1, 2, 3]\n[6, 7, 8]\n[1, 2, 3, 6, 7, 8]", regex=False).exit(0)
 
 @check50.check()
 def example2():
     """Example 2"""
-    check50.run("java Grader\"10 11 12 13 14 15 16 17\" \"11 13 15 17 19 21 23\"")\
+    check50.run("java Grader \"10 11 12 13 14 15 16 17\" \"11 13 15 17 19 21 23\"")\
             .stdout("[10, 11, 12, 13, 14, 15, 16, 17, 19, 21, 23]\n[11, 13, 15, 17]\n[10, 12, 14, 16]\n[19, 21, 23]\n[10, 12, 14, 16, 19, 21, 23]", regex=False).exit(0)
 
 @check50.check()
 def example3():
     """Example 3"""
-    check50.run("java Grader\"4 5 6 7 8 76\" \"3 4 5 6 23 46 53\"")\
+    check50.run("java Grader \"4 5 6 7 8 76\" \"3 4 5 6 23 46 53\"")\
             .stdout("[3, 4, 5, 6, 7, 8, 23, 46, 53, 76]\n[4, 5, 6]\n[7, 8, 76]\n[3, 23, 46, 53]\n[3, 7, 8, 23, 46, 53, 76]", regex=False).exit(0)
 
 @check50.check()
 def example4():
     """Empty Sets"""
-    check50.run("java Grader\"\" \"\"")\
+    check50.run("java Grader \"\" \"\"")\
             .stdout("[]\n[]\n[]\n[]\n[]", regex=False).exit(0)
 
