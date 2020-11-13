20b49743e2758728e2960c55af0b194c774142fa
mr-poston <john.poston@risd.org>
2020-11-13 18:03:03 +0000

    Add Spanish To English

---
+++ cs3/spanish/__init__.py
@@ -0,0 +1,34 @@
+import check50
+
+@check50.check()
+def exists1():
+    """SpanishToEnglish.java exists"""
+    check50.exists("SpanishToEnglish.java")
+
+@check50.check()
+def exists2():
+	"""SpanRunner.java exists"""
+	check50.exists("SpanRunner.java")
+
+@check50.check(exists1)
+def class_compiles():
+    """SpanishToEnglish.java compiles"""
+    check50.run("javac SpanishToEnglish.java").exit(0)
+
+@check50.check(exists2)
+def runner_compiles():
+    """SpanRunner.java compiles"""
+    check50.run("javac SpanRunner.java").exit(0)
+
+@check50.check()
+def example1():
+    """putEntry method works"""
+    check50.run("java Grader 1 \"ordenador computer\" \"quiero want\" \"una a\" \" virus virus\" \"yo i\"")\
+            .stdout("{ordenador=computer\nquiero=want\nuna=a\nvirus=virus\nyo=i}", regex=False).exit(0)
+
+@check50.check()
+def example1():
+    """translate method works"""
+    check50.run("java Grader 2 \"ordenador computer\" \"quiero want\" \"una a\" \" virus virus\" \"yo i\"")\
+            .stdout("i want a comuputer virus", regex=False).exit(0)
+
