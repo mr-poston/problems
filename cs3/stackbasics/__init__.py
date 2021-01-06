b7e769c80ad164bff02d89809019c436818d0325
mr-poston <60262786+mr-poston@users.noreply.github.com>
2021-01-06 10:40:49 -0600

    Update __init__.py

--- cs3/stackbasics/__init__.py
+++ cs3/stackbasics/__init__.py
@@ -29,18 +29,18 @@ def example1():
 @check50.check()
 def example2():
     """Example 2"""
-    check50.run("java Grader 1")\
+    check50.run("java Grader 2")\
             .stdout("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]\npopping all items from the stack\n10 9 8 7 6 5 4 3 2 1", regex=False).exit(0)
 
 @check50.check()
 def example3():
     """Example 3"""
-    check50.run("java Grader 1")\
+    check50.run("java Grader 3")\
             .stdout("[#, $, %, ^, *, (, ), ), _]\npopping all items from the stack\n_ ) ) ( * ^ % $ #", regex=False).exit(0)
 
 @check50.check()
 def example4():
     """Works with no-parameter constructor"""
-    check50.run("java Grader 1")\
+    check50.run("java Grader")\
             .stdout("[]\npopping all items from the stack\n", regex=False).exit(0)
 
