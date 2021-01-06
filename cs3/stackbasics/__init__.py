de1ba844667d841b046d9fb26982c2940ad5f371
mr-poston <60262786+mr-poston@users.noreply.github.com>
2021-01-06 10:53:59 -0600

    Update __init__.py

--- cs3/stackbasics/__init__.py
+++ cs3/stackbasics/__init__.py
@@ -24,23 +24,23 @@ def runner_compiles():
 def example1():
     """Example 1"""
     check50.run("java Grader 1")\
-            .stdout("[a, b, c, d, e, f, g, h, i]\npopping all items from the stack\ni h g f e d c b a", regex=False).exit(0)
+            .stdout("[a, b, c, d, e, f, g, h, i]\n\npopping all items from the stack\ni h g f e d c b a", regex=False).exit(0)
 
 @check50.check()
 def example2():
     """Example 2"""
     check50.run("java Grader 2")\
-            .stdout("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]\npopping all items from the stack\n10 9 8 7 6 5 4 3 2 1", regex=False).exit(0)
+            .stdout("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]\n\npopping all items from the stack\n10 9 8 7 6 5 4 3 2 1", regex=False).exit(0)
 
 @check50.check()
 def example3():
     """Example 3"""
     check50.run("java Grader 3")\
-            .stdout("[#, $, %, ^, *, (, ), ), _]\npopping all items from the stack\n_ ) ) ( * ^ % $ #", regex=False).exit(0)
+            .stdout("[#, $, %, ^, *, (, ), ), _]\n\npopping all items from the stack\n_ ) ) ( * ^ % $ #", regex=False).exit(0)
 
 @check50.check()
 def example4():
     """Works with no-parameter constructor"""
     check50.run("java Grader")\
-            .stdout("[]\npopping all items from the stack\n", regex=False).exit(0)
+            .stdout("[]\n\npopping all items from the stack\n", regex=False).exit(0)
 
