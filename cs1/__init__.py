7917707fecdb1f90df814fdd6082e082386dacc3
mr-poston <60262786+mr-poston@users.noreply.github.com>
2021-03-09 13:02:51 -0600

    Update __init__.py

--- cs1/__init__.py
+++ cs1/__init__.py
@@ -3,4 +3,8 @@ import check50
 @check50.check()
 def Test_has_three_names():
     """Test has three names"""
-    check50.run("python3 main.py").stdin("3\nJohn\nDavid\nPoston", prompt=True).stdout("Name: Name: Name: First name: John\nMiddle names: ['David']\nLast name: Poston\n", regex=False).exit()
+    check50.run("python3 main.py").stdin("3\nJohn\nDavid\nPoston", prompt=True) \
+    .stdin("John", prompt=True) \
+    .stdin("David", prompt=True) \
+    .stdin("Poston", prompt=True) \
+    .stdout("First name: John\nMiddle names: ['David']\nLast name: Poston\n", regex=False).exit()
