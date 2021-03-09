beb00de361845382db451cbf2eab57b16bc3de95
mr-poston <60262786+mr-poston@users.noreply.github.com>
2021-03-09 13:05:14 -0600

    Update __init__.py

--- cs1/__init__.py
+++ cs1/__init__.py
@@ -3,7 +3,7 @@ import check50
 @check50.check()
 def Test_has_three_names():
     """Test has three names"""
-    check50.run("python3 main.py").stdin("3\nJohn\nDavid\nPoston", prompt=True) \
+    check50.run("python3 main.py").stdin("3", prompt=True) \
     .stdin("John", prompt=True) \
     .stdin("David", prompt=True) \
     .stdin("Poston", prompt=True) \
