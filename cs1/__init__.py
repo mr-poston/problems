b41545f72a76566ecb25f84c2bc84ee32d2143ad
mr-poston <60262786+mr-poston@users.noreply.github.com>
2021-03-09 13:18:39 -0600

    Update __init__.py

--- cs1/__init__.py
+++ cs1/__init__.py
@@ -8,3 +8,23 @@ def Test_has_three_names():
     .stdin("David", prompt=True) \
     .stdin("Poston", prompt=True) \
     .stdout("First name: John\nMiddle names: ['David']\nLast name: Poston\n", regex=False).exit()
+    
+@check50.check()
+def Test_has_two_names():
+    """Test has two names"""
+    check50.run("python3 main.py").stdin("2", prompt=True) \
+    .stdin("Marie", prompt=True) \
+    .stdin("Antoinette", prompt=True) \
+    .stdout("First name: Marie\nMiddle names: []\nLast name: Antionette\n", regex=False).exit()
+    
+@check50.check()
+def Test_has_six_names():
+    """Test has three names"""
+    check50.run("python3 main.py").stdin("6", prompt=True) \
+    .stdin("Catherine", prompt=True) \
+    .stdin("the", prompt=True) \
+    .stdin("Great", prompt=True) \
+    .stdin("Empress", prompt=True) \
+    .stdin("of", prompt=True) \
+    .stdin("Russia", prompt=True) \
+    .stdout("First name: Catherine\nMiddle names: ['the', 'Great', 'Empress', 'of']\nLast name: Russia\n", regex=False).exit()
