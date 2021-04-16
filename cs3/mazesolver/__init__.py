8f9a4b6528cab804ea7266262b9e27b14b02b8d1
mr-poston <60262786+mr-poston@users.noreply.github.com>
2021-04-16 10:58:15 -0500

    Update __init__.py

--- cs3/mazesolver/__init__.py
+++ cs3/mazesolver/__init__.py
@@ -10,42 +10,42 @@ def class_compiles():
     """Maze.java compiles"""
     check50.run("javac Maze.java").exit(0)
 
-@check50.checks(class_compiles)
+@check50.check(class_compiles)
 def test_0():
     """Works for Maze 0"""
     check50.run("java Grader 0").stdout("exit not found", regex=False).exit(0)
 
-@check50.checks(class_compiles)
+@check50.check(class_compiles)
 def test_1():
     """Works for Maze 1"""
     check50.run("java Grader 1").stdout("exit found", regex=False).exit(0)
 
-@check50.checks(class_compiles)
+@check50.check(class_compiles)
 def test_2():
     """Works for Maze 2"""
     check50.run("java Grader 2").stdout("exit not found", regex=False).exit(0)
 
-@check50.checks(class_compiles)
+@check50.check(class_compiles)
 def test_3():
     """Works for Maze 3"""
     check50.run("java Grader 3").stdout("exit found", regex=False).exit(0)
 
-@check50.checks(class_compiles)
+@check50.check(class_compiles)
 def test_4():
     """Works for Maze 4"""
     check50.run("java Grader 4").stdout("exit found", regex=False).exit(0)
 
-@check50.checks(class_compiles)
+@check50.check(class_compiles)
 def test_5():
     """Works for Maze 5"""
     check50.run("java Grader 5").stdout("exit not found", regex=False).exit(0)
 
-@check50.checks(class_compiles)
+@check50.check(class_compiles)
 def test_6():
     """Works for Maze 6"""
     check50.run("java Grader 6").stdout("exit found", regex=False).exit(0)
 
-@check50.checks(class_compiles)
+@check50.check(class_compiles)
 def test_7():
     """Works for Maze 7"""
     check50.run("java Grader 7").stdout("exit not found", regex=False).exit(0)
