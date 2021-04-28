9805599140a88127e2a272e45934bdb58cbba804
mr-poston <60262786+mr-poston@users.noreply.github.com>
2021-04-28 09:51:40 -0500

    Update __init__.py

--- cs3/mybst/__init__.py
+++ cs3/mybst/__init__.py
@@ -13,30 +13,30 @@ def class_compiles():
 @check50.check(class_compiles)
 def test_0():
     """Everything looks good!"""
-    check50.run("java BSTRunner").stdout("Original Tree >>> \
-                                            1 2 3 4 6 8 9 10 15 16 20 25 \
-                                            \
-                                            Check whether Node with value 4 exists >>> true \
-                                            \
-                                            Delete Node with no children (2) >>> true \
-                                            1 3 4 6 8 9 10 15 16 20 25 \
-                                            \
-                                            Delete Node with one child (4) >>> true \
-                                            1 3 6 8 9 10 15 16 20 25 \
-                                            \
-                                            Delete Node with Two children (10) >>> true \
-                                            1 3 6 8 9 15 16 20 25 \
-                                            \
-                                            Final state of tree: \
-                                            1 3 6 8 9 15 16 20 25 \
-                                            \
-                                            Minimum value >>> 1 \
-                                            \
-                                            Maximum value >>> 25 \
-                                            \
-                                                    1 \
-                                                2 \
-                                                    3 \
-                                            5 \
-                                                6 \
+    check50.run("java BSTRunner").stdout("Original Tree >>>\n \
+                                            1 2 3 4 6 8 9 10 15 16 20 25\n \
+                                            \n \
+                                            Check whether Node with value 4 exists >>> true\n \
+                                            \n \
+                                            Delete Node with no children (2) >>> true\n \
+                                            1 3 4 6 8 9 10 15 16 20 25\n \
+                                            \n \
+                                            Delete Node with one child (4) >>> true\n \
+                                            1 3 6 8 9 10 15 16 20 25\n \
+                                            \n \
+                                            Delete Node with Two children (10) >>> true\n \
+                                            1 3 6 8 9 15 16 20 25\n \
+                                            \n \
+                                            Final state of tree:\n \
+                                            1 3 6 8 9 15 16 20 25\n \
+                                            \n \
+                                            Minimum value >>> 1\n \
+                                            \n \
+                                            Maximum value >>> 25\n \
+                                            \n \
+                                                    1\n \
+                                                2\n \
+                                                    3\n \
+                                            5\n \
+                                                6\n \
                                                     9", regex=False).exit(0)
